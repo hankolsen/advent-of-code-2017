1719e85de40505709f31a4aa1b97ddbe4c71a4cf
hank <henrik@hankolsen.com>
2017-12-03 20:15:55 +0100

    Indentation

--- day1.js
+++ day1.js
@@ -3,17 +3,17 @@ const fs = require('fs');
 
 fs.readFile('data/day1.txt', 'ascii', (err, data) => {
   const size = data.length;
-	const result = data
-		.split('')
-		.filter((value, index, array) => value === array[(index + 1) % size])
-		.reduce((a, b) => parseInt(a) + parseInt(b), 0);
+  const result = data
+    .split('')
+    .filter((value, index, array) => value === array[(index + 1) % size])
+    .reduce((a, b) => parseInt(a) + parseInt(b), 0);
 
-	console.log(result);
+  console.log(result);
 
-	const result2 = data
-		.split('')
-		.filter((value, index, array) => value === array[(index + size / 2) % size])
-		.reduce((a, b) => parseInt(a) + parseInt(b), 0);
+  const result2 = data
+    .split('')
+    .filter((value, index, array) => value === array[(index + size / 2) % size])
+    .reduce((a, b) => parseInt(a) + parseInt(b), 0);
 
-	console.log(result2);
+  console.log(result2);
 });
