cbdb62e2f59ec9024dda5fbbc5f96c5e3c94ae70
hank <henrik@hankolsen.com>
2017-12-03 20:17:07 +0100

    Indentation

--- day2.js
+++ day2.js
@@ -4,20 +4,20 @@ require('lodash.combinations');
 const fs = require('fs');
 
 fs.readFile('data/day2.csv', 'ascii', (err, data) => {
-	
-	const answer = data
-		.split('\n')
-		.map(line => line.split('\t'))
-		.reduce((acc, cells) => acc + Math.max(...cells) - Math.min(...cells), 0);
-	
-	console.log(answer);
 
-	const answer2 = data
-		.split('\n')
-		.map(line => line.split('\t'))
+  const answer = data
+    .split('\n')
+    .map(line => line.split('\t'))
+    .reduce((acc, cells) => acc + Math.max(...cells) - Math.min(...cells), 0);
+
+  console.log(answer);
+
+  const answer2 = data
+    .split('\n')
+    .map(line => line.split('\t'))
     .map(cells => _.combinations(cells, 2).find(pair => Math.max(...pair) % Math.min(...pair) === 0))
     .reduce((acc, pair) => acc + Math.max(...pair) / Math.min(...pair), 0);
-	
-	console.log(answer2);
+
+  console.log(answer2);
 
 });
