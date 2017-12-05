5c5e5a4dd1d99d0882a83b082dd065016baae278
hank <henrik@hankolsen.com>
2017-12-05 12:20:06 +0100

    Refactored day 5

--- day5.js
+++ day5.js
@@ -1,38 +1,30 @@
 #!/usr/bin/env node
 const { readFile } = require('./utils');
 
-readFile('data/day5.txt')
+const instructionStepper = (instructions, offsetter) => {
+	const goal = instructions.length;
+	let position = 0;
+	let steps = 0;
+
+	while(position < goal) {
+		let oldPosition = position;
+		const offset = instructions[position];
+		instructions[oldPosition] += offsetter(offset);
+		position += offset;
+		steps += 1;
+	}
+
+	return steps;
+};
+
+
+readFile('day5')
 	.then(data => {
 		const instructions = data.split('\n').map(instruction => parseInt(instruction, 10));
-		const instructions2 = [...instructions];
 
-		const goal1 = instructions.length;
-		const goal2 = instructions2.length;
-
-		let steps = 0;
-		let position = 0;
-
-		while (position < goal1) {
-			let oldPosition = position;
-			position += instructions[position];
-			instructions[oldPosition] += 1;
-			steps += 1;
-		}
-
-		console.log(steps);
-
-		steps = 0;
-		position = 0;
-
-		while (position < goal2) {
-			let oldPosition = position;
-			const offset = instructions2[position];
-			position += offset;
-			instructions2[oldPosition] += offset > 2 ? -1 : 1;
-			steps += 1;
-		}
-
-		console.log(steps);
+		// Since we need the instructions untocuhed for the second step, pass in a clone of the array
+		console.log(instructionStepper([...instructions], () => 1));
+		console.log(instructionStepper([...instructions], (offset) => offset > 2 ? -1 : 1));
 
 	})
 	.catch(err => console.log(`There was an error\n${err}`));
