17b32ed111cbd84727924d9f6a3c5d0be17b82f7
hank <henrik@hankolsen.com>
2017-12-09 01:10:58 +0100

    Added day 8

---
+++ day8/index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+const { getInput } = require('../utils');
+
+const registry = {};
+
+const parseInstruction = (instruction) => {
+  let [register, operation, value, x, comparatorRegister, condition, conditionValue] = [...instruction.split(' ')];
+
+  return {
+    action: {
+      register,
+      operation: operation === 'inc' ? 1 : -1,
+      value: parseInt(value, 10),
+    },
+    test: {
+      register: comparatorRegister,
+      operation: condition,
+      value: parseInt(conditionValue, 10),
+    },
+  };
+};
+
+const operations = {
+  '<': (a, b) => a < b,
+  '>': (a, b) => a > b,
+  '<=': (a, b) => a <= b,
+  '>=': (a, b) => a >= b,
+  '!=': (a, b) => a !== b,
+  '==': (a, b) => a === b,
+};
+
+let max = Number.MIN_VALUE;
+
+const getRegistryValue = register => registry[register] || 0;
+
+const testOperation = test => operations[test.operation](getRegistryValue(test.register), test.value);
+
+const applyOperation = ({ action, test }) => {
+  if (testOperation(test)) {
+    registry[action.register] = getRegistryValue(action.register) + (action.value * action.operation);
+    max = Math.max(max, registry[action.register]);
+  }
+};
+
+
+getInput()
+  .then((data) => {
+    data
+      .map(parseInstruction)
+      .forEach(applyOperation);
+
+    console.log(Math.max(...Object.values(registry)));
+    console.log(max);
+  })
+  .catch(err => console.log(`There was an error\n${err}`));
