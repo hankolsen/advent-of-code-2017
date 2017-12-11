0b8bc110b26cea9dfe61a19ea574c0817acf11d6
hank <henrik@hankolsen.com>
2017-12-11 17:42:59 +0100

    Work in progress day 10

--- day10/index.js
+++ day10/index.js
@@ -4,6 +4,32 @@ const { getInput } = require('../utils');
 getInput()
   .then((data) => {
     const input = data[0].split(',').map(char => parseInt(char, 10));
-    console.log(input);
+    let skipSize = 0;
+    let currentPosition = 0;
+    let listIndex = 0;
+
+    let list = [...Array(5).keys()];
+
+    for (let i = 0; i < 2; i += 1) {
+      const length = input[listIndex];
+      console.log(`CurrPos: ${currentPosition}`);
+      console.log(`Length: ${length}`);
+      console.log(`List: ${list}`);
+      console.log(`Slice start ${currentPosition}, slice size ${(currentPosition + length) % list.length} + ${Math.max(0, currentPosition - 1)}`);
+
+      const selection = [...list.slice(currentPosition, currentPosition + length), ...list.slice(0, Math.max(0, currentPosition - 1))];
+      console.log(`Selection : ${selection}`);
+      selection.reverse();
+      console.log(`Reverse : ${selection}`);
+      list = [...selection, ...list.slice(length)];
+console.log(`list ${list}`);
+      currentPosition += length + skipSize;
+      skipSize += 1;
+      listIndex += 1;
+      console.log('\n');
+    }
+
+
+    console.log(list);
   })
   .catch(err => console.log(`There was an error\n${err}`));
