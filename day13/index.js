095d39c60948d6b5f3e4ef18ed8c9a051adabede
hank <henrik@hankolsen.com>
2017-12-13 12:49:26 +0100

    Refactored day13 part 1

--- day13/index.js
+++ day13/index.js
@@ -4,45 +4,27 @@ const { getInput } = require('../utils');
 
 getInput()
   .then((data) => {
-    const input = {};
-    data.forEach((row) => {
+    const input = data.map((row) => {
       const [depth, range] = row.split(': ');
-      input[depth] = {
-        range: parseInt(range, 10),
-        direction: 1,
-        scannerPosition: 0,
-      };
+      return { depth, range: parseInt(range, 10) };
     });
 
-    const keys = Object.keys(input).map(key => parseInt(key, 10));
+    const map = input.reduce((obj, item) => Object.assign(obj, { [item.depth]: item.range }), {});
+    const keys = Object.keys(map);
     const lastStep = Math.max(...keys);
 
-    const moveScanners = (state) => {
-      keys.forEach((key) => {
-        const row = state[key];
-        row.scannerPosition += row.direction;
-        if (row.scannerPosition === row.range - 1) {
-          row.direction = -1;
-        }
-        if (row.scannerPosition === 0) {
-          row.direction = 1;
-        }
-      });
-    };
-
-
     const part1 = () => {
-      const state = { ...input };
       let currentPosition = 0;
       let caught = 0;
 
       const movePacket = () => {
-        if (keys.includes(currentPosition)) {
-          if (state[currentPosition].scannerPosition === 0) {
-            caught += currentPosition * state[currentPosition].range;
+        const range = map[currentPosition] ? map[currentPosition] : 0;
+        if (range > 0) {
+          const frequency = 2 * (range - 1);
+          if (currentPosition % frequency === 0) {
+            caught += currentPosition * range;
           }
         }
-        moveScanners(state);
         currentPosition += 1;
         while (currentPosition <= lastStep) {
           movePacket();
