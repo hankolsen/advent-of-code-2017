1bc3874ed188bc038c41bfb6f168c5e318378a8d
hank <henrik@hankolsen.com>
2017-12-13 17:48:49 +0100

    Refactored day 13 for part 2

--- day13/index.js
+++ day13/index.js
@@ -13,29 +13,43 @@ getInput()
     const keys = Object.keys(map);
     const lastStep = Math.max(...keys);
 
-    const part1 = () => {
+    const movePacket = (delay = 0, breakOnCaught = false) => {
+      let caught = false;
+      let score = 0;
       let currentPosition = 0;
-      let caught = 0;
 
-      const movePacket = () => {
-        const range = map[currentPosition] ? map[currentPosition] : 0;
+      while (currentPosition <= lastStep) {
+        const range = Math.max(map[currentPosition], 0);
         if (range > 0) {
           const frequency = 2 * (range - 1);
-          if (currentPosition % frequency === 0) {
-            caught += currentPosition * range;
+          if ((currentPosition + delay) % frequency === 0) {
+            score += currentPosition * range;
+            caught = true;
+            if (breakOnCaught) {
+              break;
+            }
           }
         }
         currentPosition += 1;
-        while (currentPosition <= lastStep) {
-          movePacket();
-        }
-      };
+      }
 
-      movePacket();
-      console.log(caught);
+      return { score, caught };
+    };
+
+    const part1 = () => {
+      console.log(movePacket().score);
+    };
+
+    const part2 = () => {
+      let delay = 0;
+      while (movePacket(delay, true).caught) {
+        delay += 1;
+      }
+      console.log(delay);
     };
 
     part1();
+    part2();
 
   })
   .catch(err => console.log(`There was an error\n${err}`));
