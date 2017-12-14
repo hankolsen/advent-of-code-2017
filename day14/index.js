f65b5110e3e39ce41b831d8ed70da1328008d4f4
hank <henrik@hankolsen.com>
2017-12-14 16:56:57 +0100

    Added day14 part 2

--- day14/index.js
+++ day14/index.js
@@ -9,22 +9,53 @@ getInput()
 
     const keyString = data[0];
 
-    const binaryInputs = [...Array(128).keys()]
+    const grid = [...Array(128).keys()]
       .map(number =>
         [...hashString(`${keyString}-${number}`)]
           .map(hexToBin)
-          .join(''));
+          .join(''))
+      .map(row => [...row].map(char => parseInt(char, 2)));
+
 
     const part1 = () => {
-
-      const result = binaryInputs
-        .reduce((total, row) => total + [...row].reduce((rowSum, number) => rowSum + parseInt(number, 10), 0), 0);
-
+      const result = grid
+        .reduce((total, row) => total + row.reduce((rowSum, number) => rowSum + number, 0), 0);
       console.log(result);
     };
 
+
     const part2 = () => {
 
+      const width = grid[0].length - 1;
+      const height = grid.length - 1;
+
+      const cleanRegion = (x, y) => {
+        if (x < 0 || x > width || y < 0 || y > height || !grid[y][x]) {
+          return;
+        }
+
+        grid[y][x] = 0;
+        cleanRegion(x + 1, y);
+        cleanRegion(x, y - 1);
+        cleanRegion(x - 1, y);
+        cleanRegion(x, y + 1);
+      };
+
+      let regions = 0;
+      let y = 0;
+
+      grid.forEach((row) => {
+        let x = 0;
+        row.forEach((char) => {
+          if (char) {
+            regions += 1;
+            cleanRegion(x, y);
+          }
+          x += 1;
+        });
+        y += 1;
+      });
+      console.log(regions);
     };
 
     part1();
