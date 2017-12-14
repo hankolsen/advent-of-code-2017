e0c9ef3ba2d4896227e15976dae5d0d808ba7393
hank <henrik@hankolsen.com>
2017-12-14 15:26:18 +0100

    Moved hashString method from day10 to utils in order to reuse for day14

--- utils.js
+++ utils.js
@@ -1,3 +1,4 @@
+/* eslint no-param-reassign: 0, no-bitwise: 0  */
 const fs = require('fs');
 
 const getInput = () => new Promise((resolve, reject) => {
@@ -10,5 +11,41 @@ const getInput = () => new Promise((resolve, reject) => {
   });
 });
 
+const listSize = 256;
 
-module.exports.getInput = getInput;
+const applyLengths = (list, lengths, currentPosition = 0, skipSize = 0) => {
+  lengths.forEach((length) => {
+    list = [...list.slice(currentPosition), ...list.slice(0, currentPosition)];
+    list = [...list.slice(0, length).reverse(), ...list.slice(length)];
+    list = [...list.slice(-currentPosition), ...list.slice(0, -currentPosition)];
+    currentPosition = (currentPosition + length + skipSize) % listSize;
+    skipSize += 1;
+  });
+  return { result: list, currentPosition, skipSize };
+};
+
+const hashString = (str) => {
+  let list = [...Array(listSize).keys()];
+  const lengths = [...[...str].map(char => char.charCodeAt(0)), ...[17, 31, 73, 47, 23]];
+  let currentPosition = 0;
+  let skipSize = 0;
+
+  Array(64).fill().forEach(() => {
+    ({ result: list, currentPosition, skipSize } = applyLengths(list, lengths, currentPosition, skipSize));
+  });
+
+  // Create XOR hash
+  let hash = '';
+  Array(16).fill().forEach(() => {
+    hash += list
+      .splice(0, 16)
+      .reduce((a, b) => a ^ b)
+      .toString(16)
+      .padStart(2, '0');
+  });
+
+  return hash;
+};
+
+
+module.exports = { applyLengths, getInput, hashString };
