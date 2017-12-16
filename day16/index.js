853465e758b727f211338aace28a262609501d6b
hank <henrik@hankolsen.com>
2017-12-16 15:47:11 +0100

    Refactored day16 part 1

--- day16/index.js
+++ day16/index.js
@@ -7,61 +7,33 @@ getInput()
 
     const input = [...Array(16).keys()].map(n => String.fromCharCode(n + 97));
     const danceMoves = data[0].split(',');
+
     const swap = (programs, a, b) => {
       [programs[a], [programs[b]]] = [programs[b], programs[a]];
       return programs;
     };
 
-    const spin = (options = { }) => {
-      let { programs, involved: x } = options;
-      x %= programs.length;
-      programs = [
-        ...[...programs.slice(-x)],
-        ...[...programs.slice(0, x)],
-        ...[...programs.slice(x, (programs.length - x))],
-      ].slice(0, programs.length);
-      return programs;
-    };
-
-    const exchange = ({ programs, involved }) => {
-      const [a, b] = involved.split('/');
-      return swap(programs, a, b);
-    };
-
-    const partner = ({ programs, involved }) => {
-      const [a, , b] = [...involved];
-      return swap(programs, programs.indexOf(a), programs.indexOf(b));
-    };
-
     const moves = {
-      s: spin,
-      x: exchange,
-      p: partner,
+      s: (programs, x) => [...[...programs.slice(-x)], ...[...programs.slice(0, (programs.length - x))]],
+      x: (programs, a, b) => swap(programs, a, b),
+      p: (programs, a, b) => swap(programs, programs.indexOf(a), programs.indexOf(b)),
     };
 
-    const dance = (danceMove, programs) => {
-      const [move, ...involved] = [...danceMove];
-      programs = moves[move]({
-        programs,
-        involved: involved.join(''),
-      });
-      return programs;
-    };
+    const dance = start => danceMoves.reduce((programs, danceMove) => {
+      const [, move, ...involved] = danceMove.match(/([sxp])(\w+)\/?(\w+)?/)
+      return moves[move](programs, ...involved);
+    }, start);
 
-    const part1 = () => {
-      let programs = input;
-      danceMoves.forEach((danceMove) => {
-        programs = dance(danceMove, programs);
-      });
-      console.log(programs.join(''));
-    };
 
+    const part1 = () => dance(input).join('');
 
     const part2 = () => {
 
     };
 
-    part1();
+
+    console.log(part1());
+
     part2();
 
   })
