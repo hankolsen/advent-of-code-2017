aef3d353f190bb6c73b30d643feeef4ca6725317
hank <henrik@hankolsen.com>
2017-12-19 22:43:35 +0100

    Added day18 part 2

--- day17/index.js
+++ day17/index.js
@@ -6,23 +6,17 @@ getInput()
   .then((data) => {
 
     const steps = parseInt(data[0], 10);
-
-    const step = (buffer, pos) => {
-      const value = buffer[pos] + 1;
-      const nextPos = ((pos + steps) % buffer.length) + 1;
-      buffer.splice(nextPos, 0, value);
-      return nextPos;
-
-    };
+    const buffer = [0];
+    let pos = 0;
 
     const part1 = () => {
-      const buffer = [0];
-      let pos = 0;
-      while (buffer[pos] < 2017) {
-        pos = step(buffer, pos);
+      for (let value = 1; value <= 2017; value += 1) {
+        pos = ((pos + steps) % buffer.length) + 1;
+        buffer.splice(pos, 0, value);
       }
       console.log(buffer[pos + 1]);
     };
+
 
     const part2 = () => {
     };
--- day18/index.js
+++ day18/index.js
@@ -1,47 +1,86 @@
 #!/usr/bin/env node
-/* eslint no-confusing-arrow: 0, no-return-assign: 0 */
+/* eslint no-param-reassign: 0, no-return-assign: 0, no-confusing-arrow: 0 */
 const { getInput } = require('../utils');
 
 getInput()
   .then((data) => {
 
     const input = data.map(row => [...row.split(' ')]);
-    const register = {};
-    let lastPlayed = '';
-    let currentRow = 0;
 
-    const getValue = value => Number.isNaN(parseInt(value, 10)) ? register[value] : parseInt(value, 10);
+    const get = (value, register) => Number.isNaN(parseInt(value, 10)) ? register[value] : parseInt(value, 10);
+    const set = (position, getValue) => ({ register, currentRow }) =>
+      ({ register: { ...register, [position]: getValue(register) }, currentRow: currentRow += 1 });
 
     const instructions = {
-      set: (position, value) => (register[position] = getValue(value)),
-      add: (position, value) => (register[position] += getValue(value)),
-      mul: (position, value) => (register[position] *= getValue(value)),
-      mod: (position, value) => (register[position] %= getValue(value)),
-      rcv: (position) => {
-        if (register[position] !== 0) {
-          console.log(`rcv: ${lastPlayed}`);
-        }
-      },
-      jgz: (position, value) => {
-        if (register[position] > 0) {
-          currentRow += (getValue(value) - 1);
-        }
-      },
-      snd: position => (lastPlayed = register[position]),
+      set: (position, value) => set(position, register => get(value, register)),
+      add: (position, value) => set(position, register => register[position] + get(value, register)),
+      mul: (position, value) => set(position, register => register[position] * get(value, register)),
+      mod: (position, value) => set(position, register => register[position] % get(value, register)),
+      rcv: position => set(position, register => register.lastPlayed),
+      jgz: (position, value) => ({ register, currentRow }) =>
+        ({ register, currentRow: currentRow += get(position, register) > 0 ? get(value, register) : 1 }),
+      snd: position => set('lastPlayed', register => register[position]),
     };
 
+
+    function* generator(id) {
+      let state = {
+        register: { p: id },
+        currentRow: 0,
+      };
+
+      while (state.currentRow < input.length) {
+        const [instruction, position, value] = input[state.currentRow];
+        if (instruction === 'rcv') {
+          state.register.lastPlayed = yield null;
+          if (!state.register.lastPlayed) {
+            break;
+          }
+        }
+        state = instructions[instruction](position, value)(state);
+        if (instruction === 'snd') {
+          yield state.register.lastPlayed;
+        }
+      }
+
+      return state;
+    }
+
+
     const part1 = () => {
-      while (true) {
-        const [instruction, position, value] = input[currentRow];
-        instructions[instruction](position, value);
-        if (instruction === 'rcv' && register[position] > 0) {
+      let state = { register: {}, currentRow: 0 };
+      while (state.currentRow < input.length) {
+        const [instruction, position, value] = input[state.currentRow];
+        state = instructions[instruction](position, value)(state);
+        if (instruction === 'rcv' && state.register[position] > 0) {
           break;
         }
-        currentRow += 1;
       }
+      console.log(state.register.lastPlayed);
     };
 
     const part2 = () => {
+      const [generator0, generator1] = [0, 1].map(id => generator(id));
+      let [queue0, queue1, result0, result1, counter] = [[], [], generator0.next(), generator1.next(), 0];
+
+      while (!result0.done || !result1.done) {
+        if (result0.value) {
+          queue1 = [result0.value, ...queue1];
+          result0 = generator0.next();
+        } else {
+          result0 = generator0.next(queue0.pop());
+        }
+
+        if (result1.value) {
+          counter += 1;
+          queue0 = [result1.value, ...queue0];
+          result1 = generator1.next();
+        } else {
+          result1 = generator1.next(queue1.pop());
+        }
+      }
+
+      console.log(counter);
     };
 
     part1();
