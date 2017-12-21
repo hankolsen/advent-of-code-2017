b61b1879ff018a4a774b8349a4f20693778205f8
hank <henrik@hankolsen.com>
2017-12-21 10:46:10 +0100

    Added day21 description and data

---
+++ day21/index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+/* eslint no-param-reassign: 0 */
+const { getRows } = require('../utils');
+
+getRows()
+  .then((data) => {
+
+    const input = data;
+
+    const part1 = () => {
+      console.log(input);
+    };
+
+    const part2 = () => {
+    };
+
+    part1();
+    part2();
+
+  })
+  .catch(err => console.log(`There was an error\n${err}`));
